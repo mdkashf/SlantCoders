fa3de1ede8563061f08b7e88a01cf68bc99d0338
Md Kashf Mashrafi <33487670+mdkashf@users.noreply.github.com>
2018-02-09 13:59:59 -0500

    Update MatrixFinder.java

--- MatrixFinder.java
+++ MatrixFinder.java
@@ -88,45 +88,6 @@ public class MatrixFinder{
 	return randArr;
     }
 
-    public static void main (String[] args){
-	
-	int[][] rand;	
-	//input for gen(int) is n, the size of the matrix
-	int size = 3000;
-	rand = gen(size);
-
-	long avgms = 0;
-	//long avgns = 0;
-	int randNum = (int)((Math.random() * 100)+ 1);
-	System.out.println(randNum);
-	int i;
-	int tenth = 0;
-	//i is the number of trials
-	for (i = 0; i < 10000; i++){
-	    if (i % 100 == 0){		
-		rand = gen(size);
-		tenth = 0;
-	    }
-	    //System.out.println("Trial #" + i);
-	    long start = System.currentTimeMillis();
-	    //long nano = System.nanoTime();
-	    
-	    find(rand, randNum);
-	    long end = System.currentTimeMillis();
-	    //long nanoEnd = System.nanoTime();
-	    tenth += (end - start);
-	    if (i % 10 == 9){
-		System.out.println((double)tenth / 10);
-	    }
-	    avgms += (end - start);
-	    //avgns += (nanoEnd - nano);
-
-	    System.out.println("total time:\n " +(end - start) + "ms\t"/* + (nanoEnd - nano) + "ns"*/);
-	}
-
-	System.out.println(i);
-	System.out.println("Average ms: " + ((double)avgms/i));
-	//System.out.println("Average ns: " + (avgns/i));
-    }
+   
 
 }
